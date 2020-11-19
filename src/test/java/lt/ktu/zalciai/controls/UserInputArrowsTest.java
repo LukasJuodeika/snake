f9a8ba6629ace379fa1a9201d3a8a3c50c0d2195
Lukas Juodeika <lukas@ito.lt>
2020-11-19 20:57:30 +0200

    Uncomment FoodFactory test and update java compatibility to 11

---
+++ src/test/java/lt/ktu/zalciai/controls/UserInputArrowsTest.java
@@ -0,0 +1,19 @@
+package lt.ktu.zalciai.controls;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserInputArrowsTest {
+
+
+
+    @Before
+    void setup() {
+        var test = 1;
+    }
+    @Test
+    void keyPressed() {
+
+    }
+}
